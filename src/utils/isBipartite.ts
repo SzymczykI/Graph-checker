b0f8a22cd000ffb248ca61ca7f4a0831c0119a8a
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-08-08 20:01:07 +0200

    again some refactoring

--- src/utils/isBipartite.ts
+++ src/utils/isBipartite.ts
@@ -1,26 +1,26 @@
 export const isBipartite = (graph: Array<number>[]) => {
-  let result = true;
-  const n = graph.length;
-  let color = Array(n).fill(true);
-  let visited = Array(n);
-  const traverse = (graph: number[][], v: number) => {
-    if (!result) return;
-    visited[v] = true;
-    for (let i of graph[v]) {
-      if (!visited[i]) {
-        color[i] = !color[v];
-        traverse(graph, i);
-      } else {
-        if (color[i] === color[v]) {
-          result = false;
-        }
+  let len = graph.length;
+  let stack = [];
+  let vis = new Array(len);
+  for (let i = 0; i < len; i++) {
+    if (vis[i]) continue;
+    vis[i] = 1;
+    stack.push(i);
+    while (stack.length) {
+      let curr;
+      curr = stack.pop();
+      let connectedNodes;
+      if (curr === undefined) return false;
+      connectedNodes = graph[curr];
+      for (let j = 0; j < connectedNodes.length; j++) {
+        let next;
+        next = connectedNodes[j];
+        if (!vis[next]) {
+          vis[next] = vis[curr] ^ 3;
+          stack.push(next);
+        } else if (vis[curr] === vis[next]) return false;
       }
     }
-  };
-  for (let v = 0; v < n; v++) {
-    if (!visited[v]) {
-      traverse(graph, v);
-    }
   }
-  return result;
+  return true;
 };
