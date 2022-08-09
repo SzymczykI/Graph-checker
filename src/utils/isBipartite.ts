040e8d5876635d66edd38965be11ecd56a9a528c
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2022-08-09 11:17:43 +0200

    refactoring

--- src/utils/isBipartite.ts
+++ src/utils/isBipartite.ts
@@ -1,27 +1,26 @@
 export const isBipartite = (graph: number[][]) => {
-    let color:number[] = [];
-    for (let i=0; i<graph.length; i++) {
-        color[i] = -1;
-    }
-    for (let i=0; i<graph.length; i++) {
-        if (color[i] === -1) {
-            let stack = [];
-            stack.push(i);
-            color[i] = 0;
-            let node;
-            while (stack.length) {
-                node = stack.pop()!;
-                for (const neighbor of graph[node]) {
-                    if (color[neighbor] === -1) {
-                        stack.push(neighbor);
-                        color[neighbor] = 1 - color[node];
-                    }
-                    else if (color[neighbor] === color[node]) {
-                        return false;
-                    }
-                }
-            }
+  let color: number[] = [];
+  for (let i = 0; i < graph.length; i++) {
+    color[i] = -1;
+  }
+  for (let i = 0; i < graph.length; i++) {
+    if (color[i] === -1) {
+      let stack = [];
+      stack.push(i);
+      color[i] = 0;
+      let node;
+      while (stack.length) {
+        node = stack.pop()!;
+        for (const neighbor of graph[node]) {
+          if (color[neighbor] === -1) {
+            stack.push(neighbor);
+            color[neighbor] = 1 - color[node];
+          } else if (color[neighbor] === color[node]) {
+            return false;
+          }
         }
+      }
     }
-    return true;
-}
+  }
+  return true;
+};
